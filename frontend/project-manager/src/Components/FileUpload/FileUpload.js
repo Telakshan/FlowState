52bb343c072b385929a6a71e3286ab69ebed42f8
Thilakshan Baskaran <telakshanb@gmail.com>
2021-04-20 23:07:23 -0400

    fixed loading component

--- frontend/project-manager/src/Components/FileUpload/FileUpload.js
+++ frontend/project-manager/src/Components/FileUpload/FileUpload.js
@@ -1,19 +1,49 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const FileUpload = (props) => {
   const [file, setFile] = useState();
   const [previewFile, setPreviewUrl] = useState();
   const [isValid, setIsValid] = useState(false);
-  const filePicker = useRef();
+  const filePickerRef = useRef();
+
+  useEffect(() => {
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
+
+  const pickFileHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  const pickedHandler = (event) => {
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (event.target.files && event.target.files.length === 1) {
+      pickedFile = event.target.files[0];
+      setFile(pickedFile);
+      setIsValid(true);
+      fileIsValid = true;
+    } else {
+      setIsValid(false);
+      fileIsValid = false;
+    }
+    props.onInput(props.id, pickedFile, fileIsValid);
+  };
 
   return (
     <div className="form-control">
       <input
         id={props.id}
-        ref={filePicker}
+        ref={filePickerRef}
         style={{ display: "none" }}
         type="file"
         accept=".jpg,.png,.jpeg,.pdf"
+        onChange={pickedHandler}
       />
     </div>
   );
